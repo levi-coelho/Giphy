62b3bae262f177216b53f6c988df6cd58b6c6dae
Levi <levi@MacBook-Pro.local>
2020-10-06 20:51:13 -0400

    end first day 2

--- src/Giphy/Client.php
+++ src/Giphy/Client.php
@@ -8,12 +8,52 @@ class Client
 {
     private $client;
     private const BASE_URI = 'api.giphy.com/v1';
+    private const API_KEY = 'MC98WNBYcHZNaOQyQvxqYQn2cFpEMOi3';
 
     public function __construct()
     {
         $this->client = new GuzzleClient([
             'base_uri' => self::BASE_URI,
             'timeout'  => 2.0,
+            'defaults'=> [
+                'query' => ['api_key' => self::API_KEY],
+            ]
         ]);
+    }
+
+
+    public function get(string $url): int
+    {
+        $response = $this->$client->request('GET', $url);
+
+        return $response->getStatusCode();
+    }
+
+    public function post(array $body, ?string $url = null): int
+    {
+        $response = $this->$client->request('POST', $url, $body);
+        
+        return $response->getStatusCode();
+    }
+
+    public function patch(): int
+    {
+        $response = $this->$client->request('PATCH', '');
+        
+        return $response->getStatusCode();
+    }
+
+    public function put(): int
+    {
+        $response = $this->$client->request('PUT', '');
+        
+        return $response->getStatusCode();
+    }
+
+    public function delete(): int
+    {
+        $response = $this->$client->request('DELETE', '');
+        
+        return $response->getStatusCode();
     }
 }
